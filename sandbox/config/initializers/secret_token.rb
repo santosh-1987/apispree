ee81ea1e721bf572971e02bed3b6b75fdddc80be
Santosh kumar Mohanty <santosh@railsfactory.org>
2012-03-20 14:58:28 +0530

    Sandbox Files

---
+++ sandbox/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sandbox::Application.config.secret_token = '6d6e811522bbe426e3d6ca5ea8595b998916445dc702218cf1d7ce21b83314f123bbfd141648746f0d635822ad917f07478240176e0f5fde86b8d4205634c324'
